4d9774118bf9d49fea1404fd824fb0f55f05c247
Sh0gunBR <121455129+Sh0gunBR@users.noreply.github.com>
2023-01-07 16:50:28 -0300

    Update bushido.sh

--- bushido.sh
+++ bushido.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
 
+#!/bin/bash
+# Rearrange screen
+clear
 figlet 'Bushido'
 echo 'By: Sh0gun'
-read=url
-./gauplus -random-agent $url -o urls.txt;
-cat urls.txt | ./urldedupe -u urls.txt -s; | ./Gxss -u * $url
-echo'/n'
-./nuclei -u $url -t nuclei-templates/vulnerabilities
+
+# Workflow
+arquivo=urls.txt
+domain=$arquivo
+./gauplus -random-agent $domain -o urls.txt ;
+cat urls.txt;
+./urldedupe -u urls.txt -s;
+./Gxss -u * $domain
+
+./nuclei -u $domain -t nuclei-templates/vulnerabilities
 
