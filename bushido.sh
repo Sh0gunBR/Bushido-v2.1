73fdf67bc5e6b33e24aac6612efcea4c4983d209
Sh0gunBR <121455129+Sh0gunBR@users.noreply.github.com>
2023-01-08 15:21:06 -0300

    Update bushido.sh

--- bushido.sh
+++ bushido.sh
@@ -6,8 +6,13 @@ echo 'By: Sh0gun' | lolcat
 
 # Workflow
 domain=$1
+if [[ domain=$1 ]]
+then
 ./gauplus -random-agent $domain --output urls.txt;
-cat urls.txt | ./urldedupe -u urls.txt -s > no_clones.txt;
-cat no_clones.txt | ./Gxss -p * $domain > clean.txt
+cat urls.txt | ./urldedupe -u  urls.txt -s > no_clones.txt;
+cat no_clones.txt | ./Gxss -p *  $domain > clean.txt
 clear
 ./nuclei -u $domain -t nuclei-templates/vulnerabilities
+else
+        echo'Use example: https://www.test.com'
+fi
